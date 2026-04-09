1fd20646430da5ae6720cfe85015a8ec5e8ae747
fixchaos <matias18nc@gmail.com>
2026-04-09 16:50:38 -0400

    feat: registrar-animal

--- src/RefugioAnimales.java
+++ src/RefugioAnimales.java
@@ -11,6 +11,8 @@ public class RefugioAnimales {
         Set<String> especies = new HashSet<>();
         Map<String, String> animalEspecie = new HashMap<>();
         String[] estados = { "Disponible", "Adoptado" };
+        Map<String, String> nombreOriginal = new HashMap<>();
+        int opcion = 0;
 
         do {
             mostrarMenu();
@@ -27,7 +29,15 @@ public class RefugioAnimales {
 
             switch (opcion) {
 
-                case 1 -> registrarAnimal(scan);
+                case 1 -> RegistrarAnimal(
+                    scan,
+                    animales,
+                    especies,
+                    estadoAnimal,
+                    animalEspecie,
+                    nombreOriginal,
+                    estados
+                );
 
                 case 2 -> registrarEspecie(scan);
 
@@ -40,6 +50,8 @@ public class RefugioAnimales {
                 case 6 -> mostrarReporte();
 
                 case 7 -> activo = false;
+
+
 
                 default -> System.out.println("Opcion no valida");
             }
@@ -58,5 +70,51 @@ public class RefugioAnimales {
         System.out.println("5. Mostrar animales adoptados");
         System.out.println("6. Mostrar reporte general");
         System.out.println("7. Salir");
+    }
+
+    public static void RegistrarAnimal(Scanner scan, List<String> animales, Set<String> especies, Map<String, String> estadoAnimal, Map<String, String> animalEspecie, Map<String, String> nombreOriginal, String[] estados) {
+        System.out.println("Ingrese nombre del animal: ");
+        String input = scan.nextLine().trim();
+        String nombre = input.toLowerCase();
+        String nombreFormateado = capitalizar(input);
+
+        if(animales.contains(nombre)) {
+            System.out.println("El animal " + nombreFormateado + " ya esta registrado.");
+            return;
+        }
+
+        System.out.print("Especie: ");
+        String especieInput = scan.nextLine().trim();
+        String especie = especieInput.toLowerCase();
+
+            if(!especies.contains(especie)){
+            System.out.println("La especie no existe.");
+            System.out.print("¿Desea registrarla? (s/n): ");
+            String respuesta = scan.nextLine();
+
+            if(respuesta.equalsIgnoreCase("s")) {
+            especies.add(especie);
+            System.out.println("Especie registrada.");
+            }else{
+            System.out.println("Registro cancelado.");
+            return;
+            }
+        }
+
+        animales.add(nombre);
+        estadoAnimal.put(nombre, estados[0]);
+
+        nombreOriginal.put(nombre, nombreFormateado);
+        animalEspecie.put(nombre, especie);
+        System.out.println("Animal registrado");
+
+
+
+
+    }
+
+    public static String capitalizar(String texto){
+        if (texto == null || texto.isEmpty()) return texto;
+        return texto.substring(0, 1).toUpperCase() + texto.substring(1).toLowerCase();
     }
 }
